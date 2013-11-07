c8b8c42af1a6c29243d168d3550ea5c83b74e636
easyScript <prezentabilan@gmail.com>
2013-11-07 01:27:00 +0100

    use bootstrap panel

--- wp-content/themes/mytheme/content.php
+++ wp-content/themes/mytheme/content.php
@@ -9,41 +9,43 @@
  * @since Twenty Twelve 1.0
  */
 ?>
-<div class="container marketing">
 
-  <div class="row">
-    <div class="col-lg-8">
-      <?php if (is_single()) : ?>
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php else : ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div class="row">
+  <div class="col-lg-8">
+    <div class="panel panel-default">
+      <div class="panel-body">
 
-          <header class="entry-header">
-            <h1 class="entry-title">
-              <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-            </h1>
-          </header>
+        <?php if (is_single()) : ?>
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php else : ?>
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-          <div class="entry-content">
-            <?php the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'mytheme')); ?>
-            <?php wp_link_pages(array('before' => '<div class="page-links">' . __('Pages:', 'mytheme'), 'after' => '</div>')); ?>
-          </div><!-- .entry-content -->
+            <header class="entry-header">
+              <h1 class="entry-title">
+                <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+              </h1>
+            </header>
 
-          <footer class="entry-meta">
-            <div class="author-description">
-              <h2><?php printf(__('About %s', 'mytheme'), get_the_author()); ?></h2>
-              <p><?php the_author_meta('description'); ?></p>
-              <div class="author-link">
-                <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" rel="author">
-                  <?php printf(__('View all posts by %s <span class="meta-nav">&rarr;</span>', 'mytheme'), get_the_author()); ?>
-                </a>
-              </div><!-- .author-link	-->
-          </footer>
-        </article>
+            <div class="entry-content">
+              <?php the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'mytheme')); ?>
+              <?php wp_link_pages(array('before' => '<div class="page-links">' . __('Pages:', 'mytheme'), 'after' => '</div>')); ?>
+            </div><!-- .entry-content -->
 
-        <hr class="featurette-divider">
-      <?php endif; // is_single() ?>
+            <footer class="entry-meta">
+              <div class="author-description">
+                <h2><?php printf(__('About %s', 'mytheme'), get_the_author()); ?></h2>
+                <p><?php the_author_meta('description'); ?></p>
+                <div class="author-link">
+                  <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" rel="author">
+                    <?php printf(__('View all posts by %s <span class="meta-nav">&rarr;</span>', 'mytheme'), get_the_author()); ?>
+                  </a>
+                </div><!-- .author-link	-->
+            </footer>
+          </article>
 
-    </div><!-- .col-lg-8	-->
-  </div><!-- .row-->
-</div><!-- .container	-->
+        <?php endif; // is_single() ?>
+
+      </div><!-- .panel-body	-->
+    </div><!-- .panel-default	-->
+  </div><!-- .col-lg-8	-->
+</div><!-- .row-->
--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The main template file
  *
@@ -14,17 +13,22 @@
  * @subpackage Twenty_Thirteen
  * @since Twenty Thirteen 1.0
  */
-get_header(); ?>
+get_header();
+?>
 
-<div id="primary" class="site-content">
-		<div id="content" role="main">
-		<?php if ( have_posts() ) : ?>
+<section id="primary" class="site-content container marketing">
+  <div id="content" role="main">
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
-      
+    <?php if (have_posts()) : ?>
+
+      <?php /* Start the Loop */ ?>
+      <?php while (have_posts()) : the_post(); ?>
+        <?php get_template_part('content', get_post_format()); ?>
+      <?php endwhile; ?>
+
     <?php endif ?>
+
+  </div><!-- #content -->
+</section><!-- #primary -->
 
 <?php get_footer(); ?>
